0e9f209517f1f51c7f2a7111c76174005217f8e5
William Leduc <1677272@bdeb.qc.ca>
2023-09-12 20:19:28 -0400

    Updated code

---
+++ TP1_ENGIN/Assets/Scripts/CharacterControllerStateMachine.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerStateMachine : MonoBehaviour
+{
+    public Camera Camera { get; private set; }
+    public Rigidbody RB { get; private set; }
+
+    [field: SerializeField]
+    public float FowardAccelerationValue { get; private set; }
+    [field: SerializeField]
+    public float SideAccelerationValue { get; private set; }
+    [field: SerializeField]
+    public float SlowingValue { get; private set; }
+    [field: SerializeField]
+    public float MaxVelocity { get; private set; }
+    [field: SerializeField]
+    public float JumpIntensity { get; private set; } = 1000.0f;
+
+    [SerializeField]
+    private CharacterFloorTrigger m_floorTrigger;
+    private CharacterState m_currentState;
+    private List<CharacterState> m_possibleStates;
+
+    private void Awake()
+    {
+        m_possibleStates = new List<CharacterState>();
+        m_possibleStates.Add(new FreeState());
+        m_possibleStates.Add(new JumpState());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = Camera.main;
+        RB = GetComponent<Rigidbody>();
+
+        foreach (CharacterState state in m_possibleStates)
+        {
+            state.OnStart(this);
+        }
+        m_currentState = m_possibleStates[0];
+        m_currentState.OnEnter();
+    }
+
+    private void Update()
+    {
+        m_currentState.OnUpdate();
+        TryStateTransition();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_currentState.OnFixedUpdate();
+    }
+
+    private void TryStateTransition()
+    {
+        if (!m_currentState.CanExit())
+        {
+            return;
+        }
+
+        //Je PEUX quitter le state actuel
+        foreach (var state in m_possibleStates)
+        {
+            if (m_currentState == state)
+            {
+                continue;
+            }
+
+            if (state.CanEnter())
+            {
+                //Quitter le state actuel
+                m_currentState.OnExit();
+                m_currentState = state;
+                //Rentrer dans le state state
+                m_currentState.OnEnter();
+                return;
+            }
+        }
+    }
+
+    public bool IsInContactWithFloor()
+    {
+        return m_floorTrigger.IsOnFloor;
+    }
+}
